439e1c27e2c58fd5ec465d950191b9c477c20a3d
syed ghouse <syedghoue.01@gmail.com>
2020-11-08 21:16:33 +0530

    commit for chef-repo
    this is learning for chef

--- .chef/config.rb
+++
@@ -1,9 +0,0 @@
-# See http://docs.chef.io/config_rb.html for more information on knife configuration options
-
-current_dir = File.dirname(__FILE__)
-log_level                :info
-log_location             STDOUT
-node_name                "ghouse01"
-client_key               "#{current_dir}/ghouse01.pem"
-chef_server_url          "https://api.chef.io/organizations/dste"
-cookbook_path            ["#{current_dir}/../cookbooks"]
--- README.md
+++
@@ -1,2 +0,0 @@
-# chef-repo
- This is for learning Chef
